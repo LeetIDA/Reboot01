4ab3fc78a7dd3eee5418af303c35e3fbc21900f9
IDA <leetida@proton.me>
2023-08-09 16:35:45 +0300

    cl-camp4

--- bh-piscine/myfamily.sh
+++ bh-piscine/myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#* export HERO_ID=1 First :)
 curl -s "https://learn.reboot01.com/assets/superhero/all.json" | jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .connections.relatives' | sed 's/"//g'
